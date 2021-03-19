e5f41ba0d5f7fca122f764b949093d85e8235871
xiaqueen <69283920+xiaqueen@users.noreply.github.com>
2021-03-19 12:01:14 +0800

    Add files via upload

--- terroform-google/main.tf
+++ terroform-google/main.tf
@@ -1,14 +1,3 @@
-variable "project" { }
-
-variable "credentials_file" { }
-
-variable "region" {
-  default = "us-central1"
-}
-
-variable "zone" {
-  default = "us-central1-a"
-}
 
 terraform {
   required_providers {
